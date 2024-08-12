042ba67b8928495d27a7860cdb89e418a5faf88d
vklepov <vklepov@ozon.ru>
2024-08-12 18:08:35 +0300

    Add async error management

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 export class ValidationError extends Error {
 }
 
+type FieldError = { name: string; message: string };
+export class MultiValidationError extends Error {
+    constructor(public readonly items: FieldError[]) {
+        super('');
+    }
+}
+
 type FieldValidator = (value: string) => void;
 type Validations = Record<string, FieldValidator>;
 
-export function leform(validations: Validations) {
+type Options = {
+    sumbit: (form: HTMLFormElement) => Promise<void>
+}
+
+export function leform(validations: Validations, ops: Options) {
+    const serverErrors = {};
+
     function validateField(el: HTMLInputElement): void {
+        const serverError = serverErrors[el.name];
+        if (serverError) {
+            return el.setCustomValidity(serverError);
+        }
         const validation = validations[el.name];
         try {
             validation && validation(el.value);
@@ -29,11 +46,22 @@ export function leform(validations: Validations) {
         }
     }
 
-    function handleSubmit(e: SubmitEvent) {
+    async function handleSubmit(e: SubmitEvent) {
         const form = e.target as HTMLFormElement;
         validateForm(form);
-        const isValid = form.reportValidity();
-        !isValid && e.preventDefault();
+        form.reportValidity();
+        e.preventDefault();
+        try {
+            await ops.sumbit(form);
+        } catch (err) {
+            if (err instanceof MultiValidationError) {
+                err.items.forEach(({ name, message }) => {
+                    serverErrors[name] = message;
+                    validateField(form.elements[name]);
+                });
+                form.reportValidity();
+            }
+        }
     }
     function handleInvalid(e: Event) {
         const form = (e.target as HTMLInputElement)?.form!;
@@ -44,7 +72,9 @@ export function leform(validations: Validations) {
         firstInvalid.scrollIntoView({ behavior: 'smooth' });
     }
     function handleInput(e: Event) {
-        validateField(e.target as HTMLInputElement);
+        const target = e.target as HTMLInputElement;
+        serverErrors[target.name] = null;
+        validateField(target);
         syncCustomError(e);
     }
 
--- src/sample.ts
+++ src/sample.ts
@@ -1,10 +1,18 @@
-import { leform, ValidationError } from "./index";
+import { leform, MultiValidationError, ValidationError } from "./index";
 
 const paymentForm = leform({
     receiver: (v) => {
         if (!v.startsWith('40817')) {
             throw new ValidationError('Account number must start with 40817');
         }
+    }
+}, {
+    sumbit: () => {
+        return new Promise((_, fail) => {
+            setTimeout(() => {
+                fail(new MultiValidationError([{ name: 'receiver', message: 'account blocked' }]));
+            }, 1000);
+        });
     }
 });
 
